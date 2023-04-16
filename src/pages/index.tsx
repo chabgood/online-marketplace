60f4be36510882a6579696bcd05e932a5a559c6a
Chris Habgood <chabgood@gmail.com>
2023-04-15 22:16:41 -0500

    added listing model, navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
@@ -7,9 +6,6 @@ import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
-  const user = useUser();
-
   return (
     <>
       <Head>
@@ -22,14 +18,6 @@ const Home: NextPage = () => {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
-          { user.isSignedIn ? (
-           <SignOutButton>
-              <span className="text-white">Sign out</span>
-            </SignOutButton>) : (
-            <SignInButton>
-              <span className="text-white">Sign in</span>
-            </SignInButton>
-          )}
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
